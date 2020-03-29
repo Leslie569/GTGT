8336deaf2632220a51f541287bd8d67a5c1cc8b3
Elaine Xu <jxu443@gatech.edu>
2020-03-29 16:48:52 -0400

    duel

---
+++ covid19/src/sample/AlertBox.java
@@ -0,0 +1,41 @@
+package sample;
+
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+
+public class AlertBox {
+
+    public static void display(String title, String message) {
+        display(title, message, "got it");
+    }
+
+    public static void display(String title, String message, String buttonText) {
+        Stage window = new Stage();
+
+        //Block events to other windows
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.setTitle(title);
+        window.setMinWidth(500);
+        window.setMinHeight(300);
+
+        Label label = new Label();
+        label.setText(message);
+        label.setWrapText(true);
+        label.setMaxWidth(250);
+        Button closeButton = new Button(buttonText);
+        closeButton.setOnAction(e -> window.close());
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(label, closeButton);
+        layout.setAlignment(Pos.CENTER);
+
+        //Display window and wait for it to be closed before returning
+        Scene scene = new Scene(layout);
+        window.setScene(scene);
+        window.showAndWait();
+    }
+
+}
