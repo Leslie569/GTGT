5d7d499aad1c366874260643054981d1eb493d95
Elaine Xu <jxu443@gatech.edu>
2020-03-28 20:45:52 -0400

    data visulization

--- covid19/src/sample/Controller.java
+++ covid19/src/sample/Controller.java
@@ -1,4 +1,39 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+
+import java.net.*;
+import java.util.ResourceBundle;
+
+
+public class Controller implements Initializable {
+    @FXML private CategoryAxis x;
+    @FXML private NumberAxis y;
+    @FXML private LineChart<?,?> LineChart;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        nationalButtonAction();
+    }
+
+    @FXML
+    private void nationalButtonAction() {
+        XYChart.Series series = new XYChart.Series();
+
+        series.getData().add(new XYChart.Data("1", 80));
+        series.getData().add(new XYChart.Data("2", 100));
+        series.getData().add(new XYChart.Data("3", 200));
+        series.getData().add(new XYChart.Data("4", 500));
+        series.getData().add(new XYChart.Data("5", 1000));
+        series.getData().add(new XYChart.Data("6", 2000));
+        series.getData().add(new XYChart.Data("7", 4000));
+
+        LineChart.getData().addAll(series);
+    }
 }
+
